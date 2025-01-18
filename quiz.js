7e9765bd3524a313f62f78e36d3e5926d84624ec
Thabelo Tshishonga <thabelo.tshishonga@gmail.com>
2025-01-18 11:37:22 +0200

    fix

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         document.getElementById("feedback").textContent = "Correct! Well done.";
     } else {
         document.getElementById("feedback").textContent = "That's incorrect. Try again!";
